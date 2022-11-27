c1fe6bc4a7654849649db0824839840798803f60
Telickers <hello@telickers.net>
2022-11-27 09:42:52 +0200

    add analytics

--- pages/_app.js
+++ pages/_app.js
@@ -16,6 +16,7 @@ export default MyApp;
 function PageHead() {
   return (
     <Head>
+      <script async src="https://cdn.splitbee.io/sb.js"></script>
       <title>حين نتعارف...ماذا نسأل؟</title>
       <meta property="og:title" content="حين نتعارف...ماذا نسأل؟" />
       <meta
